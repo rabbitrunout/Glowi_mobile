bdf6e22f260a5afdb811b6632cb5e673d4336bd4
Irina Saf <irina.safronova0801@gmail.com>
2026-04-02 23:12:01 -0400

    Initial Commit

---
+++ Glowi_mobile/Glowi_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glowi_mobileApp.swift
+//  Glowi_mobile
+//
+//  Created by Irina Saf on 2026-04-02.
+//
+
+import SwiftUI
+
+@main
+struct Glowi_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
